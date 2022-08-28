8e7dd11161b6bcb4e354d1153db88635f98becab
Rodrigo Lanfri <rlanfri@gmail.com>
2022-08-28 18:45:41 -0300

    Cities/City Dinamico
    Modificar datos para que sean dinámicos (falta BACKGROUNDIMAGE del ACORDEON

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -20,32 +20,32 @@ import { GET_CITY_BY_ID, GET_ALL_CITIES, ORDER_PACKAGES } from "./action";
         }
       case ORDER_PACKAGES:
         let packagesOrdered
-            if(payload === 'sort') packagesOrdered = state.packages;
-            if(payload === 'ascendant by name') {
+            if(action.payload === 'sort') packagesOrdered = state.packages;
+            if(action.payload === 'ascendant by name') {
               packagesOrdered = state.packages.sort(function(a, b) {
                     if(a.name > b.name) return 1;
                     else if(a.name < b.name) return -1;
                     else return 0;
                 })} 
-            else if(payload === 'descendant by name') {
+            else if(action.payload === 'descendant by name') {
               packagesOrdered = state.packages.sort(function(a, b) {
                     if(a.name > b.name) return -1;
                     else if(a.name < b.name) return 1;
                     else return 0;
                 })} 
-            else if(payload === 'ascendant by price') {
+            else if(action.payload === 'ascendant by price') {
               packagesOrdered = state.packages.sort(function(a, b) {
                     return a.price - b.price;
                 })}
-            else if(payload === 'descendant by price') {
+            else if(action.payload === 'descendant by price') {
               packagesOrdered = state.packages.sort(function(a, b) {
                     return b.price - a.price;
                 })}
-            else if(payload === 'ascendant by score') {
+            else if(action.payload === 'ascendant by score') {
               packagesOrdered = state.packages.sort(function(a, b) {
                     return a.score - b.score;
                 })}
-            else if(payload === 'descendant by score') {
+            else if(action.payload === 'descendant by score') {
               packagesOrdered = state.packages.sort(function(a, b) {
                     return b.score - a.score;
                 })}
