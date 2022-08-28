dcf0509d0098b741750a74588676c6e87fe95cb8
Rodrigo Lanfri <rlanfri@gmail.com>
2022-08-28 20:24:05 -0300

    PruebasSinfruto

--- src/components/Filters/FilterPackages.jsx
+++ src/components/Filters/FilterPackages.jsx
@@ -32,6 +32,7 @@ useEffect(() => {
                 <form id='formFilter'> 
                 
                 <p> ORDER BY </p>
+                
                 <select onChange={e => handleOrder(e)}>
                     <option hidden>ALFABETIC</option>
                     <option value='sort'>Sort</option>
@@ -53,7 +54,7 @@ useEffect(() => {
                     <option value='descendant by score'>DESC</option>
                 </select>
                 
-                <p> FILTER RESULTS </p>
+               
                 <p> CITIES </p>
                 <select onChange={e => handleFilterByCity(e)}>
                     <option value='all'>All</option>
